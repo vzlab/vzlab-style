629bfe083b84d1291046321c34d6e8ba8ff7542f
Luiz “Felds” Liscia <luiz.felds@vetorzero.com.br>
2017-04-19 11:30:33 -0300

    add grid

--- dist/js/header-shadow.js
+++ dist/js/header-shadow.js
@@ -1,15 +1,14 @@
 ;(function () {
-    // to check:
-    // parseFloat(getComputedStyle(parentElement).fontSize);
-
-    var full_shadow = 120
+    var full_shadow = 256
     var update_shadow = false
     var header_element = document.querySelector('#header')
-    var max_shadow_size = 32;
+    var max_shadow_size = 24;
 
     self.addEventListener('scroll', function (e) {
         update_shadow = true
     })
+
+    // header_element.style.transition = "100ms box-shadow 500ms"
 
     function updateHeaderShadow() {
         if (update_shadow) {
