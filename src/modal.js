8749bfbfa8c3537e9238b3b04f3765013f65f7f6
Filipe Morais <fi.morais.mail@gmail.com>
2019-02-19 14:49:32 -0300

    Basic modal functionality.

--- src/modal.js
+++ src/modal.js
@@ -1,19 +1,34 @@
 const body = document.querySelector('body');
-const modal = document.querySelector('.vz-modal');
-const btnModal = document.querySelector('#btn-modal');
-const closeModalButton = document.querySelector('.vz-modal--close');
+const modal = [...document.querySelectorAll('.vz-modal')];
+const closeModalButton = [...document.querySelectorAll('[data-dismiss]')];
 
 const closeModal = (e) => {
-  body.style.overflow = 'auto';
-  modal.style.display = 'none';
+  modal.map(item => {
+    body.style.overflow = 'auto';
+    item.style.display = 'none';
+  })
 }
 
 const openModal = (e) => {
-  if (e.target.dataset.target === modal.id) {
-    body.style.overflow = 'hidden';
-    modal.style.display = 'block';
+  modal.map(item => {
+    if (e.target.dataset.target === item.id) {
+      body.style.overflow = 'hidden';
+      item.style.display = 'block';
+    }
+  });
+
+  return;
+}
+
+const handleKeyInput = (e) => {
+  if (e.key === 'Escape') {
+    closeModal();
   }
 }
 
-btnModal.addEventListener('click', openModal);
-closeModalButton.addEventListener('click', closeModal);
+document.addEventListener('click', openModal);
+document.addEventListener('keydown', handleKeyInput);
+
+closeModalButton.map(button => {
+  button.addEventListener('click', closeModal);
+});
